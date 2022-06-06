76047f86b21de0d6f1d3bf513d7f582784d4de4e
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-06 14:40:36 +0530

    [Harini]: Mood Analyser Created

---
+++ MoodAnalyser/MoodAnalyser/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser();
+checkMood.AnalyseMood("I am in sad mood");
