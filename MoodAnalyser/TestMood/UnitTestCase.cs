71417c512ad647ae987d9e434f4470f0c8d1a36a
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-08 16:29:09 +0530

    [Harini]: Invalid Method Name Passed Throw Exception

--- MoodAnalyser/TestMood/UnitTestCase.cs
+++ MoodAnalyser/TestMood/UnitTestCase.cs
@@ -133,6 +133,22 @@ namespace TestMood
             string findMood = Mood_Analyser.MoodAnalyserFactory.InvokeMoodAnalyser("happy", "AnalyseMood");
             Assert.AreEqual(expected, findMood);
         }
+        
+        [Test]
+        public void GivenHappyinInvalidMood_ThrowException()
+        {
+            try
+            {
+                string expected = "Happy";
+                string findMood = Mood_Analyser.MoodAnalyserFactory.InvokeMoodAnalyser("happy", "Analyse");
+                Assert.AreEqual(expected, findMood);
+            }
+            catch(Exception e )
+            {
+                Assert.AreEqual("Method Not Found", e.Message);
+            }
+        }
+
     }
 }
 
